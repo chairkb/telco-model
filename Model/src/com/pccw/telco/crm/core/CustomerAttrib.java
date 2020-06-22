b35d0a75046b0083f158c3131892751a95930d1b
Chair <kokbeng.chair@pccw.com>
2020-06-22 19:52:59 +0800

    To add all the getter and setter of CustmerAttrib and create its db

--- Model/src/com/pccw/telco/crm/core/CustomerAttrib.java
+++ Model/src/com/pccw/telco/crm/core/CustomerAttrib.java
@@ -1,6 +1,11 @@
 package com.pccw.telco.crm.core;
 
+import biz.shujutech.base.App;
+import biz.shujutech.base.Connection;
+import biz.shujutech.base.Hinderance;
 import biz.shujutech.db.object.Clasz;
+import biz.shujutech.db.object.ObjectBase;
+import biz.shujutech.db.relational.BaseDb;
 import biz.shujutech.db.relational.FieldType;
 import biz.shujutech.reflect.ReflectField;
 
@@ -13,6 +18,41 @@ public class CustomerAttrib extends Clasz {
 	@ReflectField(type=FieldType.BOOLEAN, displayPosition=30) public static String Visible;
 
 
+
+
+
+
+
+	public static final String PROPERTY_LOCATION_APP = "C:\\Users\\USER4\\pccw\\crm-ddl\\Model\\src\\com\\pccw\\telco\\crm\\core\\telco-core.properties";
+	public static void main(String[] args) throws Exception {
+		ObjectBase objectDb = new ObjectBase();
+		Connection conn = null;
+		try {
+			String[] args1 = { PROPERTY_LOCATION_APP };
+			objectDb.setupApp(args1);
+			objectDb.setupDb();
+			conn = objectDb.getConnPool().getConnection();
+
+			String INIT_SCHEMA = "INIT_SCHEMA";
+			String CREATE_MODEL = "CREATE_MODEL";
+
+			String actionType = INIT_SCHEMA;
+
+			if (actionType.equals(INIT_SCHEMA)) {
+
+				CustomerAttrib customerAttrib;
+				customerAttrib = (CustomerAttrib) ObjectBase.CreateObject(conn, CustomerAttrib.class);
+				
+			}
+
+		} catch (Exception ex) {
+			App.logEror(new Hinderance(ex, "Application encounter fatal error, application is aborting...."));
+		} finally {
+			if (conn != null) {
+				objectDb.getConnPool().freeConnection(conn);
+			}
+		}		
+	}
 
 	
 }
